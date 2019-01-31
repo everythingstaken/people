dae413bc305a8c32ba01cf5dc92c43419539c406
denniscarr <dcarr1@gmail.com>
2019-01-30 19:34:49 -0500

    you know whatever ;)

---
+++ Assets/scripts/TheScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheScript : MonoBehaviour {
+
+	int waifuLoveMeter = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		waifuLoveMeter++;
+	}
+}
